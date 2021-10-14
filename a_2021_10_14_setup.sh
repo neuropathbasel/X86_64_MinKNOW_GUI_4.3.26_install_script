789affcd27178422a4cb6a6fd69ca93cf4f6e932
Neuropathology Lab at Institute for Medical Genetics and Pathology, University Hospital Basel, Switzterland <juergen.hench@usb.ch>
2021-10-14 16:11:38 +0200

    Add files via upload

---
+++ a_2021_10_14_setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#collection of commands to be carried out directly after fresh install of xUbuntu 18.04
+#using a local screen, keyboard and mouse. Note after this script the login GUI is dectivated
+#A GUI for this machine will be only be available via VNC
+#to be executed from the directory the
+#get system to most rescent state
+sudo apt-get update
+sudo apt-get upgrade
+
+#synaptic gnome disk, open ssh, vnc4server
+#logged in as user
+sudo apt-get install synaptic gnome-disk-utility openssh-server build-essential vnc4server xfce4 xfce4-goodies
+
+#setup vnc - dialogue box should be requested
+echo "please enter vnc password for user minknow"
+vncpasswd
+
+#copy working xstartup to .vnc directory
+cp add_config_files/xstartup ~/.vnc/
+
+#create script folder for starting vnc server:
+mkdir ~/scripts/
+cp  add_config_files/vnc_start.sh ~/scripts
+chmod +x ~/scripts/vnc_start.sh
+
+#installation of nvidia driver using nvidia mirror
+#echo "hello world" >> my_file.txt
+
+wget -qO - https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub | sudo apt-key add -
+
+echo "#NVIDA" | sudo tee -a /etc/apt/sources.list
+echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /" | sudo tee -a /etc/apt/sources.list
+
+#download relevant data from NVIDIA repository
+
+sudo apt update
+sudo apt upgrade
+#should show
+sudo apt install nvidia-driver-470 cuda
+
+#To disable the GUI for login - otherwise the loginscreen will be running on the NVIDIA GPU
+sudo systemctl set-default multi-user.target
+sudo reboot
---
+++ b_2021_10_13_minknow_guppy_install_deb.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#note: there must be no /var/lib/minknow/data directory - not as a physical or as a symbolic linked director.
+#if this directory exists, remove it or rename it before starting the installation
+
+sudo apt-get install -f
+sudo dpkg -i ont-jwt-auth_0.28-1~bionic_amd64.deb
+sudo dpkg -i minknow-core-minion-nc_4.3.12-bionic_amd64.deb
+sudo usermod -aG sudo minknow
+sudo usermod -aG user minknow
+sudo dpkg -i ont-bream4-minion_6.2.6-1~bionic_all.deb
+sudo dpkg -i ont-configuration-customer-minion_4.3.11-1~bionic_all.deb
+sudo dpkg -i ont-kingfisher-ui-minion_4.3.26-1~bionic_all.deb
+sudo dpkg -i ont-vbz-hdf-plugin_1.0.0-1~bionic_amd64.deb
+sudo dpkg -i ont-guppy-cpu-for-minion_5.0.13-1~bionic_amd64.deb
+sudo apt-get install -f
+sudo dpkg -i ont-guppyd-for-minion_5.0.13-1~bionic_all.deb
+sudo dpkg -i minion-nc_21.06.10-1~bionic_all.deb
+#sudo service minknow status
+#sudo service minknow start
+#sudo service minknow status
+#nvidia-smi
+
+#GUPPy in GPU mode - extracted form local tar.gz archive
+sudo mkdir /applications
+sudo chmod a=rwx /applications/
+#cd ~/Downloads/MinKNOW_4.3.26_20210929_x86_64_1804
+
+#one way - via ONT Downloads
+#wget https://mirror.oxfordnanoportal.com/software/analysis/ont-guppy_5.0.14_linux64.tar.gz
+#placed a copy in this debian collection
+#direct unpacking to /applications
+tar xvf ont-guppy_5.0.14_linux64.tar.gz -C /applications
+
+#rename /applications/ont-guppy directory
+sudo mv -T /applications/ont-guppy /applications/ont-guppy_5.0.14_linux64_GPU
+
+#reconfiguration so that GPU guppy is us used
+sudo service minknow stop
+sudo service guppyd stop
+
+#replace guppyd.service config file
+sudo cp add_config_files/guppyd.service /lib/systemd/system/guppyd.service
+sudo rm -rf /etc/systemd/system/guppyd.service
+sudo rm -rf /etc/systemd/system/guppyd.service.d
+sudo ln -s /lib/systemd/system/guppyd.service /etc/systemd/system/
+
+sudo mkdir /data
+sudo chown minknow /data
+sudo mv /var/lib/minknow/data /var/lib/minknow/data_old
+sudo ln -s /data /var/lib/minknow/data
+
+#sudo systemctl daemon-reload
+#sudo service guppyd start
+#nvidia-smi
+sudo reboot
