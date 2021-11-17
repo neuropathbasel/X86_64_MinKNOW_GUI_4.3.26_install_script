6c1173e5b31f6b8e407d4987b4f2ae9d8e7036e3
Juergen Hench <juergen.hench@usb.ch>
2021-11-17 18:29:04 +0100

    update

--- gita_X_86.sh
+++ gita_X_86.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 echo "This will push everything in "`pwd`" to GitHub..."
+git remote set-url origin git@github.com:neuropathbasel/X86_64_MinKNOW_GUI_4.3.26_install_script
 git add -A
 git commit -m "update"
-git push https://git@github.com:neuropathbasel/X86_64_MinKNOW_GUI_4.3.26_install_script
+git push
